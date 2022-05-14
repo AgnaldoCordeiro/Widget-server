8994676c5dc64fd9234f608196d7a87924724197
AgnaldoCordeiro <agske13@gmail.com>
2022-05-14 11:42:00 -0300

    extended limit

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false, limit: '2mb' }))
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
