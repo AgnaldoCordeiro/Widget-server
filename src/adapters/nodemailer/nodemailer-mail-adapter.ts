c084efad579b60941a81404be47fba53a3bcae94
AgnaldoCordeiro <agske13@gmail.com>
2022-05-31 17:05:46 -0300

    adicinando serviço de email

--- src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -2,11 +2,10 @@ import { MailAdapter, SendMailData } from '../mail-adapters'
 import nodemailer from 'nodemailer'
 
 const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",  
-  port: 2525,  
+  host: "gmail",   
   auth: {
-    user: "9869996fb1aca3",
-    pass: "2611780fb46f41"    
+    user: "twfeedbacks@gmail.com",
+    pass: "ag5323896"    
   }
 });
 
@@ -14,7 +13,7 @@ export class NodemailerMailAdapter implements MailAdapter {
   async sendMail({subject, body}: SendMailData) {
    
   await transport.sendMail({
-    from: 'Equipe Feedget <cpd@twimobiliaria.com.br>',
+    from: 'Equipe Feedback <twfeedbacks@gmail.com>',
     to: 'Agnaldo Cordeiro <cpd@twimobiliaria.com.br>',
     subject: subject,
     html: body,
