3d7abaeda7d43372cda5c5c9fb4afbfb2c57119a
AgnaldoCordeiro <agske13@gmail.com>
2022-06-15 16:43:12 -0300

    arrumando teste boleto

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
+var fs = require('fs');
 
 
 export const routes = express.Router();
@@ -21,6 +22,23 @@ routes.post('/feedbacks', async (req, res) => {
   return res.status(201).send()
 })
 
- 
+routes.get('/download', function (req, res) {
+  const file = "/public/Boleto-12345.pdf";
+  res.download(file); // Set disposition and send it.
+});
+const cpf = "42333150821"
 
+routes.post(`/${cpf}`, function (req, res) {
+  var file = fs.createReadStream("./public/Boleto-12345.pdf");
+  var stat = fs.statSync('./public/Boleto-12345.pdf');
+
+  res.setHeader('Content-Length', stat.size);
+    res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', 'attachment; filename=table1.pdf');
+  file.pipe(res)
+  res.download(file);
+});
+
+ 
+   
 
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import { routes } from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(routes)
-
-app.get('/download', function(req, res){
-  const file = "/upload-folder/Boleto-12345.pdf";
-  res.download(file); // Set disposition and send it.
-});
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('listening on');
